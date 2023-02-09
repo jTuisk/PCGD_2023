5c2df0500773f0f512446e96a6609c25f8d85222
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2023-02-09 18:20:14 +0200

    More gui changes

--- Assets/ButtonHider/TurnButtonHider.cs
+++ Assets/ButtonHider/TurnButtonHider.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnButtonHider : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject endTurnButton;
     public GameObject DrawEventCard;
-    public GameObject deckImage;
+    public Image deckImage;
+    public Image EventdeckImage;
+    public TMPro.TextMeshProUGUI deckText;
     public GameObject discardPileImage;
+
     void Start()
     {
         
@@ -21,13 +25,17 @@ public class TurnButtonHider : MonoBehaviour
         if (Deck.Instance.inBattle)
         {
             endTurnButton.SetActive(true);
-            deckImage.SetActive(true);
+            deckImage.gameObject.SetActive(true);
+            EventdeckImage.gameObject.SetActive(false);
             discardPileImage.SetActive(true);
+            deckText.text="";
         }
         else {
-            deckImage.SetActive(false);
+            deckImage.gameObject.SetActive(false);
+            EventdeckImage.gameObject.SetActive(true);
             discardPileImage.SetActive(false);
             endTurnButton.SetActive(false);
+            deckText.text="Cards left in event deck "+Deck.Instance.EventDeck.Count;
         }
 
         if (!Deck.Instance.eventVisible&& !Deck.Instance.inBattle)
