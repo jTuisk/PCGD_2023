8fecccc312ac74e374444f92a603ca9a50729929
cherr66 <cherchen999@gmail.com>
2023-02-24 23:04:37 +0200

    add draw card animation

--- Assets/Cards/EventCards/EventCard.cs
+++ Assets/Cards/EventCards/EventCard.cs
@@ -13,9 +13,23 @@ public class EventCard : MonoBehaviour
     public GameObject Menu;
     public GameObject ButtonPrefab;
     public List<EventCardMenuItem> options;
-    // Start is called before the first frame update
     public GameObject CreatureBase;
     public CreatureDataContainer Creature;
+    public GameObject eventCardFront;
+    public GameObject eventCardBack;
+    public GameObject deckAnchor;
+
+    #region Animation Parameters
+    private Vector3 slideStartRotation = new Vector3(0f, 0f, 18.45f);
+    private Vector3 slideEndRotation = Vector3.zero;
+    private float cardMinimalSize = 0.15f;
+    private float cardNormalSize = 1.0f;
+    private Vector3 flipUpAngle = new Vector3(90f, 0f, 0f);
+    private Vector3 flipFlatAngle = Vector3.zero;
+
+    private float slideAnimDuration = 0.5f;
+    private float flipAnimDuration = 0.5f;
+    #endregion
 
 
     public void CreateEventCard(EventCardData data) {
@@ -114,10 +128,123 @@ public class EventCard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void Start()
+    {
+        CardBornFromDeck();
     }
 
 
+    #region Animation
+    
+    private void CardBornFromDeck()
+    {
+        // Calculate the deck_image's local position under cardBack's parent
+        // First, get deck_image localPosition under its parent Canvas
+        Vector2 deckImageLocalPos;
+        var eventDeckImage = GameObject.Find("Canvas/EventDeck Image");
+        if(eventDeckImage != null)
+        {
+            deckImageLocalPos = eventDeckImage.transform.localPosition;
+        }
+        else
+        {
+            deckImageLocalPos = deckAnchor.transform.position;
+        }
+        
+        // Second, covert localPosition to worldPosition according to EventCard's canvas
+        // Which is different from deck_image's world position
+        var canvasObj = this.gameObject.transform.GetComponentInChildren<Canvas>().gameObject;
+        var deckImageWorldPos = canvasObj.transform.TransformPoint(deckImageLocalPos);
+        
+        // Finally, convert worldPosition to localPosition according to cardBack's parent Rect
+        Vector2 deckRelativeLocalPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            eventCardBack.transform.parent.GetComponent<RectTransform>(),
+            Camera.main.WorldToScreenPoint(deckImageWorldPos),
+            Camera.main,
+            out deckRelativeLocalPos
+        );
 
+        eventCardBack.transform.localPosition = deckRelativeLocalPos;
+        eventCardBack.transform.localScale = new Vector3(cardMinimalSize, cardMinimalSize, cardMinimalSize);
+        eventCardBack.transform.eulerAngles = slideStartRotation;
+        
+        eventCardFront.SetActive(false);
+        eventCardBack.SetActive(true);
+
+        StartCoroutine("CardSlideToCenter");
+
+        // TODO audio
+    }
+
+    IEnumerator CardSlideToCenter() 
+    {
+        var slideStartTime = Time.time;
+        var slideStartPos = eventCardBack.transform.localPosition;
+
+        do {
+            eventCardBack.transform.localPosition = Vector3.Lerp(
+                slideStartPos, 
+                eventCardFront.transform.localPosition,
+                (Time.time - slideStartTime)/slideAnimDuration);
+
+            eventCardBack.transform.eulerAngles = Vector3.Lerp(
+                slideStartRotation, slideEndRotation,
+                (Time.time - slideStartTime)/slideAnimDuration);
+
+            yield return new WaitForEndOfFrame();
+        } while ((Time.time - slideStartTime) <= slideAnimDuration);
+        
+        StartCoroutine("CardFlip");
+    }
+
+
+    IEnumerator CardFlip()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        var flipStartTime = Time.time;
+
+        Vector3 startScale = new Vector3(cardMinimalSize, cardMinimalSize, cardMinimalSize);
+        Vector3 endScale = new Vector3(cardNormalSize, cardNormalSize, cardNormalSize);
+        eventCardFront.transform.localScale = startScale;
+        eventCardFront.transform.eulerAngles = flipUpAngle;
+        eventCardFront.SetActive(false);
+
+        do {
+            eventCardBack.transform.localScale = Vector3.Lerp(
+                startScale, endScale,
+                (Time.time - flipStartTime)/flipAnimDuration);
+            eventCardFront.transform.localScale = Vector3.Lerp(
+                startScale, endScale,
+                (Time.time - flipStartTime)/flipAnimDuration);
+
+            if((Time.time - flipStartTime) < (flipAnimDuration*0.5f))
+            {
+                eventCardBack.transform.eulerAngles = Vector3.Lerp(
+                flipFlatAngle, flipUpAngle,
+                (Time.time - flipStartTime)/(flipAnimDuration * 0.5f));
+            }
+            else
+            {
+                eventCardFront.SetActive(true);
+                eventCardBack.SetActive(false);
+                eventCardFront.transform.eulerAngles = Vector3.Lerp(
+                    flipUpAngle, flipFlatAngle,
+                    (Time.time - flipStartTime - flipAnimDuration * 0.5f)/(flipAnimDuration * 0.5f));
+            }
+
+            yield return null;
+        } while ((Time.time - flipStartTime) <= flipAnimDuration);
+
+        // Make sure interpolation outcome
+        eventCardFront.transform.localScale = endScale;
+        eventCardFront.transform.eulerAngles = flipFlatAngle;
+    }
+
+    #endregion
 
 }
