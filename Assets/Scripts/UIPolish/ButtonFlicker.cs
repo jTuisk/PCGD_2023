a72af9b9f00c901ecde154137154f733f0cc7ff7
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2023-04-28 00:00:15 +0300

    Draw event button scale now also pulses

--- Assets/Scripts/UIPolish/ButtonFlicker.cs
+++ Assets/Scripts/UIPolish/ButtonFlicker.cs
@@ -12,10 +12,13 @@ public class ButtonFlicker : MonoBehaviour
     public Color normalColor;
     public Color flickerColor;
     public bool Flickering=true;
+    public Vector3 maxScale=new Vector3(1.3f,1.3f,1.3f);
+    private Vector3 minScale;
     void Start()
     {
         image = GetComponent<Image>(); // get the image component
         timer = 0f; // reset the timer
+        minScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -26,6 +29,12 @@ public class ButtonFlicker : MonoBehaviour
             timer += Time.deltaTime * flickerSpeed; // increase the timer by delta time times flicker speed
             float t = Mathf.Abs(Mathf.Sin(timer)); // get a value between 0 and 1 based on the sine of the timer
             image.color = Color.Lerp(normalColor, flickerColor, t); // interpolate between normal and flicker colors based on t
+            transform.localScale = Vector3.Lerp(minScale, maxScale, t);
         }
+        else
+        {
+            transform.localScale = minScale;
+        }
+       
     }
 }
